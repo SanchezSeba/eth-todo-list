443f41d5f1f69355554ac811b4b6ffef145bea58
Sebastian Sanchez <sebastian.sanchez@synaptic.cl>
2021-06-05 21:37:22 -0400

    Created basic todo-list contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,21 @@
 pragma solidity ^0.5.0;
 contract TodoList {
 	uint public taskCount = 0;
+
+	constructor() public {
+    	createTask('First Task!!');
+    }
+
+	struct Task {
+    	uint id;
+    	string content;
+    	bool completed;
+  	}
+
+  	mapping(uint => Task) public tasks;
+
+  	function createTask(string memory _content) public {
+    	taskCount ++;
+    	tasks[taskCount] = Task(taskCount, _content, false);
+  	}
 }
